f5283de953d34e0470f0b6cdb440e760b5c6fb80
Jorge Leon <jorgeleonpicture@gmail.com>
2022-03-30 11:13:15 +0000

    primer commit con Navbar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,13 @@
 import React from "react";
+import Navbar from "./navbar";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	let navBackground = "bg-dark";
+	let navNa = "navbar-dark";
+
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Navbar bgNav={navBackground} nav={navNa} />
 		</div>
 	);
 };
